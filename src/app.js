ff4f9f2482eca045213117919079fece7553e5d2
gabriel-victor933 <gabrielvictor933@usp.br>
2023-04-06 12:08:05 -0300

    modificando arrays de dados

--- src/app.js
+++ src/app.js
@@ -2,15 +2,9 @@ import express from "express"
 import cors from "cors"
 
 
-const users = [{
-    	username: 'bobesponja', 
-    	avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png" 
-    }]
+const users = []
 
-const tweets = [{
-	username: "bobesponja",
-    tweet: "Eu amo hambúrguer de siri!"
-}]
+const tweets = []
 
 const app = express()
 app.use(cors())
